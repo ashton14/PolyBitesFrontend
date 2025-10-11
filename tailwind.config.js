040504f63f636520de9232ea36c5b49aff589252
ashton14 <ashtonalonge14@gmail.com>
2025-10-11 13:16:05 -0700

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'heading': ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } 
