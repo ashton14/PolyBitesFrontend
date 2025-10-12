592348829759fdd65a25e50b5f2e9ffc8a1c277e
ashton14 <ashtonalonge14@gmail.com>
2025-10-11 22:57:04 -0700

    update url

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const API_BASE_URL = 'https://polybitesbackend-production.up.railway.app'
 //export const API_BASE_URL = 'http://localhost:5000';
 
 // Frontend URL (for password reset redirects, etc.)
-export const FRONTEND_URL = 'https://poly-bites-frontend.vercel.app';
+export const FRONTEND_URL = 'https://www.polybites.org/';
 //export const FRONTEND_URL = 'http://localhost:3000';
 
 // Helper to build API URLs
